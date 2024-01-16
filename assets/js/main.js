8864d86d4134da3d39847a4bc9bf9199e6547987
Sayooj Shagra <gotcandybychance@gmail.com>
2024-01-16 17:12:07 +0530

    carousel added in web potfolio

--- assets/js/main.js
+++ assets/js/main.js
@@ -24,4 +24,10 @@ var div = document.createElement("div");
     div.innerHTML='<div class="black_wall"></div><div class="loader"></div>',
     document.body.insertBefore(div,document.body.firstChild),window.onload=function() {
     document.getElementById("preloader").classList.add("off")
+
+    //Carousel
+    $(document).ready(function(){
+        $('.carousel').carousel();
+      });
+      
 };
